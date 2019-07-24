89c38a7f013dd6fec83660edc53f91bf9fef31c0
psydeep-ops <sobig.f2@gmail.com>
2019-07-24 19:00:18 +0700

    add .sh

---
+++ triangle.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Стороны треугольника через пробел:"
+read a1 b1 c1
+echo ---OK---
+#echo $a $b $c
+
+type() {
+
+    a=$1
+    b=$2
+    c=$3
+    if [ -z "$a" -o -z "$b" -o -z "$c" ]; then
+        echo "Не задана одна или более сторон трегуольника!"
+        echo "Empty var" >&2
+        return 1
+    fi
+
+    let ab=$a+$b
+    let bc=$b+$c
+    let ca=$c+$a
+
+    if [ ! $ab -gt $c -o ! $bc -gt $a -o ! $ca -gt $b ]
+    then echo "Треугольник не существует!"
+    else
+        echo "Треугольник существует!"
+        if [ $a -eq $b -a $b -eq $c ]
+            then echo "Треугольник равносторонний"
+        elif [ $a -eq $b -o $b -eq $c -o $c -eq $a ]
+            then echo "Треугольник равнобедренный"
+        else
+            echo "Треугольник обычный"
+        fi
