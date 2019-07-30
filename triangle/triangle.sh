232d6b28ff5407f3fc57b883338457f0796fe0cc
Sergey P <sobig.f2@gmail.com>
2019-07-30 22:52:44 +0700

    rewrite with bc

--- triangle/triangle.sh
+++ triangle/triangle.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #echo "Стороны треугольника через пробел:"
 read a1 b1 c1
 #echo ---OK---
@@ -18,18 +18,33 @@ type() {
 #    let ab=$a+$b
 #    let bc=$b+$c
 #    let ca=$c+$a
-    ab=$(($a+$b))
-    bc=$(($b+$c))
-    ca=$(($c+$a))
+    ab=$(echo "$a+$b" | bc -l)
+    bc=$(echo "$b+$c" | bc -l)
+    ca=$(echo "$c+$a" | bc -l)
 
-    if [ ! $ab -gt $c -o ! $bc -gt $a -o ! $ca -gt $b ]
+#    if [ ! "$ab" -gt "$c" -o ! "$bc" -gt "$a" -o ! "$ca" -gt "$b" ]
 #    then echo "Треугольник не существует!"
-    then echo "Impossible triangle" >&2
+#    then echo "Impossible triangle" >&2
+#    else
+#        echo "Треугольник существует!" >&2
+#        if [ "$a" -eq "$b" -a "$b" -eq "$c" ]
+#            then echo "Равносторонний треугольник"
+#        elif [ "$a" -eq "$b" -o "$b" -eq "$c" -o "$c" -eq "$a" ]
+#            then echo "Равнобедренный треугольник"
+#        else
+#            echo "Обычный треугольник"
+#        fi
+#
+#    fi11;20M11;20M11;20M
+    if [ $(echo "$ab <= $c || $bc <= $a || $ca <= $b" | bc -l) -eq "1" ]
+#    then echo "Треугольник не существует!"
+#    then echo "Impossible triangle" >&2
+    then return 1
     else
         echo "Треугольник существует!" >&2
-        if [ $a -eq $b -a $b -eq $c ]
+        if [ $(echo "$a == $b && $b == $c" |bc -l) -eq "1" ]
             then echo "Равносторонний треугольник"
-        elif [ $a -eq $b -o $b -eq $c -o $c -eq $a ]
+        elif [ $(echo "$a == $b || $b == $c || $c == $a" | bc -l) -eq "1" ]
             then echo "Равнобедренный треугольник"
         else
             echo "Обычный треугольник"
